db9c6667b382beb3bd7ccadeea17449fd9fd86e6
alex cammeraat <alex-cammeraat@hotmail.com>
2023-06-15 08:23:04 +0100

    Got Javascript working.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let toggleNav = function(){ //anonymous function
+let toggleNavStatus = false
+let toggleNav = function() {
     let getSidebar = document.querySelector(".nav-sidebar");
     let getSidebarUl = document.querySelector(".nav-sidebar ul");
     let getSidebarTitle = document.querySelector(".nav-sidebar span");
@@ -6,8 +7,9 @@ let toggleNav = function(){ //anonymous function
 
     if (toggleNavStatus === false) {
         getSidebarUl.style.visibility = "visible";
+        getSidebarUl.style.opacity = "1";
         getSidebar.style.width = "272px"; 
-        getSidebarTitle.style.opacity = "0.5";
+        getSidebarTitle.style.opacity = "1";
 
         let arrayLenght = getSidebarLinks.lenght; 
         for (let i = 0; i < arrayLenght; i++){
